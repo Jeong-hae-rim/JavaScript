d53b1db2b863799f5b9be90857f7430d49bfd86b
jeong-hae-rim <usemono33@gmail.com>
2020-08-03 12:51:27 +0900

    javascript TIL

--- 90/08/js/register-create.js
+++ 90/08/js/register-create.js
@@ -3,8 +3,24 @@ function newRegister() {
 	var userName = document.querySelector("#userName");					
 	var newText = document.createTextNode(userName.value);  // 새 텍스트 노드 만들기
 	newP.appendChild(newText);  // 텍스트 노드를 p 요소의 자식 요소로 연결하기
+
+	var delBttn = document.createElement("span");
+	var delText = document.createTextNode("X");
+	delBttn.setAttribute("class", "del");
+	delBttn.appendChild(delText);
+	newP.appendChild(delBttn);
         
-	var nameList = document.querySelector("#nameList");  
-	nameList.appendChild(newP); // p 요소를 #nameList의 자식 요소로 만들기
+	var nameList = document.querySelector("#nameList");
+	nameList.insertBefore(newP, nameList.childNodes[0]);
+	//nameList.appendChild(newP); // p 요소를 #nameList의 자식 요소로 만들기
 	userName.value = "";  // 텍스트 필드 지우기
-}			
+
+	var removeBttns = document.querySelectorAll(".del");
+	for(var i = 0; i<removeBttns.length; i++){
+		removeBttns[i].addEventListener("click", function(){
+			if(this.parentNode.parentNode){
+				this.parentNode.parentNode.removeChild(this.parentNode);
+			}
+		})
+	}
+}
